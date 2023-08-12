b66f275dc2e13ecab2a055385353ec1aea5398c7
Wu Tingfeng <wutingfeng@outlook.com>
2023-08-12 20:58:32 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# ChongLuaDao Phishing Blocklist
+
+![Python](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)
+![GitHub Actions](https://img.shields.io/badge/GitHub_Actions-2088FF?style=for-the-badge&logo=github-actions&logoColor=white)
+
+[![GitHub license](https://img.shields.io/badge/LICENSE-BSD--3--CLAUSE-GREEN?style=for-the-badge)](LICENSE)
+[![scraper](https://img.shields.io/github/actions/workflow/status/elliotwutingfeng/ChongLuaDao-Phishing-Blocklist/scraper.yml?branch=main&label=SCRAPER&style=for-the-badge)](https://github.com/elliotwutingfeng/ChongLuaDao-Phishing-Blocklist/actions/workflows/scraper.yml)
+![Total Blocklist URLs](https://tokei-rs.onrender.com/b1/github/elliotwutingfeng/ChongLuaDao-Phishing-Blocklist?label=Total%20Blocklist%20URLS&style=for-the-badge)
+
+Machine-readable `.txt` blocklist of phishing URLs and IP Addresses from the [Chống Lừa Đảo](https://chongluadao.vn) project, updated once a day.
+
+The URLs and IP Addresses in this blocklist are compiled by the **Chống Lừa Đảo** project.
+
+**Disclaimer:** _This project is not sponsored, endorsed, or otherwise affiliated with the Chống Lừa Đảo project._
+
+## Blocklist download
+
+| File | Download |
+|:-:|:-:|
+| urls.txt | [:floppy_disk:](urls.txt?raw=true) |
+| urls-ABP.txt | [:floppy_disk:](urls-ABP.txt?raw=true) |
+| urls-UBO.txt | [:floppy_disk:](urls-UBO.txt?raw=true) |
+| urls-UBL.txt | [:floppy_disk:](urls-UBL.txt?raw=true) |
+| urls-pihole.txt | [:floppy_disk:](urls-pihole.txt?raw=true) |
+| ips.txt | [:floppy_disk:](ips.txt?raw=true) |
+
+## Requirements
+
+- Python >= 3.11
+
+## Setup instructions
+
+`git clone` and `cd` into the project directory, then run the following
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+```bash
+python3 scraper.py
+```
+
+## Libraries/Frameworks used
+
+- [tldextract](https://github.com/john-kurkowski/tldextract)
+
+&nbsp;
+
+<sup>These files are provided "AS IS", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, arising from, out of or in connection with the files or the use of the files.</sup>
+
+<sub>Any and all trademarks are the property of their respective owners.</sub>
---
+++ scraper.py
@@ -0,0 +1,140 @@
+"""Extract phishing URLs found at https://chongluadao.vn
+and write them to a .txt blocklist
+"""
+from __future__ import annotations
+import ipaddress
+import logging
+import re
+import socket
+from datetime import datetime
+
+import requests
+import tldextract
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def current_datetime_str() -> str:
+    """Current time's datetime string in UTC
+
+    Returns:
+        str: Timestamp in strftime format "%d_%b_%Y_%H_%M_%S-UTC".
+    """
+    return datetime.utcnow().strftime("%d_%b_%Y_%H_%M_%S-UTC")
+
+
+def clean_url(url: str) -> str:
+    """Remove zero width spaces, leading/trailing whitespaces, trailing slashes,
+    and URL prefixes from a URL
+
+    Args:
+        url (str): URL.
+
+    Returns:
+        str: URL without zero width spaces, leading/trailing whitespaces, trailing slashes,
+    and URL prefixes.
+    """
+    removed_zero_width_spaces = re.sub(r"[\u200B-\u200D\uFEFF]", "", url)
+    removed_leading_and_trailing_whitespaces = removed_zero_width_spaces.strip()
+    removed_trailing_slashes = removed_leading_and_trailing_whitespaces.rstrip("/")
+    removed_https = re.sub(r"^[Hh][Tt][Tt][Pp][Ss]:\/\/", "", removed_trailing_slashes)
+    removed_http = re.sub(r"^[Hh][Tt][Tt][Pp]:\/\/", "", removed_https)
+
+    return removed_http
+
+
+def extract_phishing_urls() -> set[str]:
+    """Extract phishing URLs from chongluadao.vn phishing URL API
+
+    Returns:
+        set[str]: Unique phishing URLs.
+    """
+    try:
+        response = requests.get("https://api.chongluadao.vn/v1/blacklist", timeout=120)
+        if response.status_code != 200:
+            logger.error("API response status code: %d", response.status_code)
+            raise OSError("Unable to retrieve API response")
+
+        body = response.json()
+        urls = set(
+            clean_url(entry["url"].replace("*.", "").replace("/*", ""))
+            for entry in body
+            if "url" in entry
+        )
+        return urls
+    except Exception as error:
+        logger.error(error)
+        return set()
+
+
+if __name__ == "__main__":
+    urls: set[str] = extract_phishing_urls()
+    ips: set[str] = set()
+    non_ips: set[str] = set()
+    fqdns: set[str] = set()
+    registered_domains: set[str] = set()
+    if urls:
+        for url in urls:
+            res = tldextract.extract(url)
+            registered_domain, domain, fqdn = (
+                res.registered_domain,
+                res.domain,
+                res.fqdn,
+            )
+            if domain and not fqdn:
+                # Possible IPv4 Address
+                try:
+                    socket.inet_pton(socket.AF_INET, domain)
+                    ips.add(domain)
+                except socket.error:
+                    # Is invalid URL and invalid IP -> skip
+                    pass
+            elif fqdn:
+                non_ips.add(url)
+                fqdns.add(fqdn)
+                registered_domains.add(registered_domain)
+
+    if not non_ips and not ips:
+        logger.error("No content available for blocklists.")
+    else:
+        non_ips_timestamp: str = current_datetime_str()
+        non_ips_filename = "urls.txt"
+        with open(non_ips_filename, "w") as f:
+            f.writelines("\n".join(sorted(non_ips)))
+            logger.info(
+                "%d non-IPs written to %s at %s",
+                len(non_ips),
+                non_ips_filename,
+                non_ips_timestamp,
+            )
+
+        ips_timestamp: str = current_datetime_str()
+        ips_filename = "ips.txt"
+        with open(ips_filename, "w") as f:
+            f.writelines("\n".join(sorted(ips, key=ipaddress.IPv4Address)))
+            logger.info(
+                "%d IPs written to %s at %s", len(ips), ips_filename, ips_timestamp
+            )
+
+        fqdns_timestamp: str = current_datetime_str()
+        fqdns_filename = "urls-pihole.txt"
+        with open(fqdns_filename, "w") as f:
+            f.writelines("\n".join(sorted(fqdns)))
+            logger.info(
+                "%d FQDNs written to %s at %s",
+                len(fqdns),
+                fqdns_filename,
+                fqdns_timestamp,
+            )
+
+        registered_domains_timestamp: str = current_datetime_str()
+        registered_domains_filename = "urls-UBL.txt"
+        with open(registered_domains_filename, "w") as f:
+            f.writelines("\n".join(f"*://*.{r}/*" for r in sorted(registered_domains)))
+            logger.info(
+                "%d Registered Domains written to %s at %s",
+                len(registered_domains),
+                registered_domains_filename,
+                registered_domains_timestamp,
+            )
